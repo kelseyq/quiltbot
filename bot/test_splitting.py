0087a83765c1e03725128b60f0a9b99614b5d303
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-11-30 21:41:41 -0800

    add some stats

--- bot/test_splitting.py
+++ bot/test_splitting.py
@@ -8,13 +8,19 @@ with open('../cleaned_quiltdata/items.json', 'r') as f:
     items = json.load(f)
     names_total = 0
     names_with_links = 0
+    unique_links = set()
+    unique_names = set()
     millis_start = int(round(time.time() * 1000))
     for block_number in range(1, len(items)):
         try:
             block_data = next((item for item in items if item['block_number'] == ('%05d' % block_number)))
             names = block_data['names']
             names_total = names_total + len(names)
-            names_with_links = names_with_links + sum(1 for name in names if name.get('link'))
+            for name in names:
+                unique_names.add(name.get('normalized'))
+                if name.get('link'):
+                    unique_links.add(name.get('link'))
+                    names_with_links += 1
             tweet_block.split_names(names)
             if block_number % 100 == 0:
                 print("testing blocks " + str(block_number) + " to " + str(block_number + 100))
@@ -26,4 +32,6 @@ with open('../cleaned_quiltdata/items.json', 'r') as f:
     print("Time elapsed: " + str(elapsed) + "ms")
     print("Names: " + str(names_total))
     print("Links: " + str(names_with_links))
+    print("Unique links: " + str(len(unique_links)))
+    print("Unique names: " + str(len(unique_names)))
     print("Percent: " + str(names_with_links/names_total * 100) + "%")
