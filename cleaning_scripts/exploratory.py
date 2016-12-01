c5fe1ba547e4aa9fa80020f54c6e1f8fcc9839d6
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-11-30 21:07:57 -0800

    miscellaneous scripts in exploratory

--- cleaning_scripts/exploratory.py
+++ cleaning_scripts/exploratory.py
@@ -25,7 +25,7 @@ with open('../cleaned_quiltdata/items.json', 'r') as f:
     for item in items:
         for name in item['names']:
             if name['name'].count(',') > 1:
-                #print(name['name'])
+                # print(name['name'])
                 i = i + 1
     print(str(i), "names with more than one comma")
 
@@ -36,7 +36,7 @@ with open('../cleaned_quiltdata/items.json', 'r') as f:
     for item in items:
         for name in item['names']:
             if re.search(r'[\x80-\xFF]', name['name']):
-                #print(item['block_number'] + ": " + name)
+                #print(name['name'])
                 i = i + 1
     print(str(i) + (" names with special characters"))
 
@@ -47,20 +47,56 @@ with open('../cleaned_quiltdata/items.json', 'r') as f:
     i = 0
     for item in items:
         for name in item['names']:
-            pair = re.search(r'(.+) & (.+)', name['name'])
+            pair = re.search(r'(.+) and (.+)', name['name'])
             if pair:
                 i = i + 1
-                #print(item['block_number'] + ": " + name['name'])
+               # print(item['block_number'] + ": " + name['name'])
     print(str(i) + " names with 'and'")
 
-#duplicate names in BAR obits
-with open('../cleaned_quiltdata/bar-obit-items.json', 'r') as f:
+# #duplicate names in BAR obits
+# with open('../cleaned_quiltdata/bar-obit-items.json', 'r') as f:
+#     obits = json.load(f)
+#     dupes = 0
+#     for idx, obit in enumerate(obits):
+#         name = sorted(obit['full_name'])
+#         for i in range(idx + 1, len(obits)):
+#             if sorted(obits[i]['full_name']) == name:
+#                 print(obit['full_name'])
+#                 dupes = dupes + 1
+#     print(str(dupes), "duplicate obit names")
+
+# #duplicate names in TOP obits
+# with open('../cleaned_quiltdata/top-obit-items.json', 'r') as f:
+#     obits = json.load(f)
+#     dupes = 0
+#     for idx, obit in enumerate(obits):
+#         name = obit['title_name']
+#         for i in range(idx + 1, len(obits)):
+#             if (obits[i]['title_name']) == name:
+#                 print(obit['title_name'])
+#                 dupes = dupes + 1
+#     print(str(dupes), "duplicate obit names")
+
+# names with more than 3 components in TOP obits
+with open('../cleaned_quiltdata/top-obit-items.json', 'r') as f:
     obits = json.load(f)
-    dupes = 0
+    longnames = 0
     for idx, obit in enumerate(obits):
-        name = sorted(obit['full_name'])
-        for i in range(idx + 1, len(obits)):
-            if sorted(obits[i]['full_name']) == name:
-                print(obit['full_name'])
-                dupes = dupes + 1
-    print(str(dupes), "duplicate obit names")
+        if "&" in obit['title_name'] or (" AND " in obit['title_name']) or (" and " in obit['title_name']):
+            pass
+        else:
+            names = obit['title_name'].split()
+            if len(names) > 2:
+                # print(obit['title_name'])
+                longnames = longnames + 1
+    print(str(longnames), "long names")
+
+#composite obits
+with open('../cleaned_quiltdata/top-obit-items.json', 'r') as f:
+    obits = json.load(f)
+    andnames = 0
+    for idx, obit in enumerate(obits):
+        if "&" in obit['title_name'] or (" AND " in obit['title_name']) or (" and " in obit['title_name']):
+            #print(obit['title_name'], obit['link'])
+            andnames = andnames + 1
+    print(str(andnames), "composite names")
